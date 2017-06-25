ab0a5786c7b2b8eaee81f3ff4d2b0acdae19b526
summer <wpsumsun@126.com>
2017-06-25 23:15:40 +0800

    邮箱重置密码&返回登录

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,12 +1,13 @@
 import React , {Component} from 'react';
 import './UserDialog.css';
-import {signUp,signIn} from './leanCloud';
+import {signUp,signIn,sendPasswordResetEmail} from './leanCloud';
 
 export default class UserDialog extends Component{
     constructor(props){
         super(props)
         this.state={
-            selected:'signUp',
+            selected:'signUp',//'signIn'
+            selectedTab:'signInOrSignUp',// 'forgotPassword'
             formData:{
                 email:'',
                 username:'',
@@ -101,29 +102,71 @@ export default class UserDialog extends Component{
                 </div>
                 <div className="row actions">
                     <button type="submit">登录</button>
-                    <a href="javascript:;">忘记密码？</a>
+                    <a href="javascript:;" onClick={this.showForgotPassword.bind(this)}>忘记密码？</a>
                 </div>
             </form>
         )
-        return (
-            <div className="UserDialog-Wrapper">
-                <div className="UserDialog">
-                    <nav onChange={this.switch.bind(this)}>
-                        <label>
-                            <input type="radio" value="signUp" 
-                            checked={this.state.selected==='signUp'}
-                            onChange={this.switch.bind(this)} />注册</label>
-                        <label>
+        let  signInOrSignUp=(
+            <div className="signInOrSignUp">
+                <nav>
+                    <label>
+                        <input type="radio" value="signUp" 
+                        checked={this.state.selected === 'signUp'}
+                        onChange={this.switch.bind(this)}
+                        /> 注册</label>
+                    <label>
                         <input type="radio" value="signIn" 
-                        checked={this.state.selected==='signIn'}
-                        onChange={this.switch.bind(this)} />登录</label>
-                    </nav>
-                    <div className="panes">
-                        {this.state.selected==='signUp'?signUpForm:null}
-                        {this.state.selected==='signIn'?signInForm:null}
-                    </div>
+                        checked={this.state.selected === 'signIn'}
+                        onChange={this.switch.bind(this)}
+                        /> 登录</label>
+                </nav>
+                <div className="panes">
+                    {this.state.selected === 'signUp' ? signUpForm : null}
+                    {this.state.selected === 'signIn' ? signInForm : null}
                 </div>
             </div>
         )
+
+        let forgotPassword=(
+            <div className="forgotPassword">
+                <h3>重置密码</h3>
+                <form className="forgotPassword" onSubmit={this.resetPassword.bind(this)}>
+                    <div className="row">
+                        <label>邮箱</label>
+                        <input type="text" value={this.state.formData.email}
+                        onChange={this.changeFormData.bind(this, 'email')}/>
+                    </div>
+                    <div className="row actions">
+                        <button type="submit">发送重置邮件</button>
+                        <a href="javascript:;" onClick={this.returnToSignIn.bind(this)}>返回登录</a>
+                    </div>
+                </form>
+            </div> 
+        )
+
+        return (
+            <div className="UserDialog-Wrapper">
+                <div className="UserDialog">
+                    {this.state.selectedTab==='signInOrSignUp'?signInOrSignUp:forgotPassword}
+                </div>
+            </div>
+        )
+    }
+
+    showForgotPassword(){
+        let stateCopy=JSON.parse(JSON.stringify(this.state))
+        stateCopy.selectedTab='forgotPassword'
+        this.setState(stateCopy)
+    }
+
+     returnToSignIn(){
+        let stateCopy = JSON.parse(JSON.stringify(this.state))
+        stateCopy.selectedTab = 'signInOrSignUp'
+        this.setState(stateCopy)
+   }
+
+    resetPassword(e){
+        e.preventDefault()
+        sendPasswordResetEmail(this.state.formData.email)
     }
 }
