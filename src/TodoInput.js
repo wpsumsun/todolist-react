db49ee27b3ef5035dd91fad786b816668789640d
Summer <wpsumsun@126.com>
2017-06-30 15:35:03 +0800

    就算一个组件有方法，也可以改写成一个函数

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,20 +1,23 @@
 import React , {Component} from 'react';
 import './TodoInput.css';
 
-export default class TodoList extends Component{
-    render(){
-        return <input type='text' 
+
+function submit(props,e){
+    if(e.key==='Enter'){
+        props.onSubmit(e)
+    }
+}
+function changeTitle(props,e){
+    props.onChange(e)
+}
+
+export default function(props){
+     return(
+         <input type='text' 
         className="TodoInput"
-        defaultValue={this.props.content} 
-        onChange={this.changeTitle.bind(this)}
-        onKeyPress={this.submit.bind(this)}/>
-    }
-    submit(e){
-        if(e.key==='Enter'){
-            this.props.onSubmit(e)
-        }
-    }
-    changeTitle(e){
-        this.props.onChange(e)
-    }
+        value={props.content} 
+        onChange={changeTitle.bind(null,props)}
+        onKeyPress={submit.bind(null,props)}
+        />
+     ) 
 }
